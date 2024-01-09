2bf2d252e6a785b0bb5a7b1d364e37166d384930
ragnarops <angelriera1796@gmail.con>
2024-01-09 02:03:45 +0100

    add vars styles and theme colors, and navbar

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,13 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'codeRagnarok';
-export const SITE_DESCRIPTION = 'diseño y desarrollo de software '; 
+export const SITE_DESCRIPTION = 'diseño y desarrollo de software ';
+
+
+export const pages = [
+    "home",
+    "services",
+    "portfolio",
+    "about",
+    "contact",
+]
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,53 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+			'xs': '320px', // => @media (min-width: 640px) { ... }
+			'sm': '575px', // => @media (min-width: 640px) { ... }
+			'md': '768px', // => @media (min-width: 768px) { ... }
+			'lg': '1024px', // => @media (min-width: 1024px) { ... }
+			'xl': '1280px', // => @media (min-width: 1280px) { ... }
+			'2xl': '1536px', // => @media (min-width: 1536px) { ... }
+		  },
+		  colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			//black
+			'black': '#191919',
+			'black-dark': '#060709',
+			'black-light': '#242728',
+			//gray
+			'gray': '#919190',
+			'gray-dark': '#89827A',
+
+			//white
+			'white': '#CBCBCB',
+			'white-light': '#D3D3D3',
+	
+			//primary
+			'primary': '#464AA9',
+			'primary-light': '#4A6CE2',
+			'primary-dark': '#2E328E',
+
+			//secondary
+			'secondary': '#005C94',
+			'secondary-dark': '#E44F26',
+
+			//danger
+			'danger': '#B7455A',
+			'danger-light': 'danger-800',
+			//pink
+			'pink': '#A259FF',
+			'pink-light': '#6E36E5',
+
+			//blue
+			'blue': '#0090F9',
+			'blue-dark': '#1572B6',
+
+		  },
+		extend: {
+
+		},
 	},
 	plugins: [],
 }
